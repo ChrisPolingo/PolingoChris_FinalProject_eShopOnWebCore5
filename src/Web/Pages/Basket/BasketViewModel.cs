02f1b21165359fd9b100214c05f404706e3aef71
ChrisPolingo <95453086+ChrisPolingo@users.noreply.github.com>
2022-12-12 12:49:17 -0600

    Added basic support for tax and grand total to the basket when going to check out. Modified BasketViewModel.cs, adding two new methods of TotalTax() and GrandTotal(). Modified Checkout.cshtml by adding "Tax" and "Grand Total" titles and displays for the basket.

--- src/Web/Pages/Basket/BasketViewModel.cs
+++ src/Web/Pages/Basket/BasketViewModel.cs
@@ -14,5 +14,21 @@ namespace Microsoft.eShopWeb.Web.Pages.Basket
         {
             return Math.Round(Items.Sum(x => x.UnitPrice * x.Quantity), 2);
         }
+
+        /*
+         * Returns the total amount of tax on the current total of items in the basket
+         */
+        public decimal TotalTax()
+        {
+            return Math.Round(Total() * 0.06m, 2);
+        }
+
+        /*
+         * Returns the Grand Total of the basket, being the sum of Total() and TotalTax().
+         */
+        public decimal GrandTotal()
+        {
+            return Total() + TotalTax();
+        }
     }
 }
